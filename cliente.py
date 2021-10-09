68f9b48958787cf8e37954eee09affa8b0c2ff40
stevenmch95 <80663763+stevenmch95@users.noreply.github.com>
2021-10-08 23:17:59 -0600

    util
    util

--- cliente.py
+++ cliente.py
@@ -1,5 +1,10 @@
 import util as util
 from os import system
+import os
+import threading
+import time
+
+flag = True
 
 
 def pintarMenu():
@@ -12,7 +17,8 @@ def pintarMenu():
     print("\t**        Presione una opción        **")
     print("\t**                                   **")
     print("\t**    1 - Ingresar ruta              **")
-    print("\t**    2 - Salir                      **")
+    print("\t**    2 - Iniciar Hilo Cliente       **")
+    print("\t**    3 - Salir                      **")
     print("\t**                                   **")
     print("\t**                                   **")
     print("\t***************************************")
@@ -21,8 +27,21 @@ def pintarMenu():
     return input("\tDigite una opcion: ")
 
 
-if __name__ == '__main__':
-    if (pintarMenu() == '1'):
-        ruta = input("\tDigite la ruta: ")
-        util.escribirArchivo('ruta.txt','')
+def leerRuta():
+    while(flag == True):
+        time.sleep(1)
+        if(os.path.exists('ruta.txt')):
+            util.escribirArchivo('bitacora.txt', '\n[1,2,3,0]')
+            time.sleep(5)
+            os.remove("ruta.txt")
 
+
+if __name__ == '__main__':
+    hiloCLiente = threading.Thread(target=leerRuta)
+    hiloCLiente.start()
+    while(flag == True):
+        if (pintarMenu() == '1'):
+            ruta = input("\tDigite la ruta: ")
+            util.escribirArchivo('ruta.txt', ruta)
+        elif (pintarMenu() == '1'):
+            flag = False
