71cb02fd9f8c9bf2ddc3bac506c33257ad4c847b
JohannaGuillen <80302583+JohannaGuillen@users.noreply.github.com>
2021-10-08 23:30:04 -0600

    d
    d

--- cliente.py
+++ cliente.py
@@ -1,5 +1,10 @@
 import util as util
 from os import system
+import os
+import threading
+import time
+
+flag = True
 
 
 def pintarMenu():
@@ -12,7 +17,8 @@ def pintarMenu():
     print("\t**        Presione una opción        **")
     print("\t**                                   **")
     print("\t**    1 - Ingresar ruta              **")
-    print("\t**    2 - Salir                      **")
+    print("\t**    2 - Iniciar Hilo Cliente       **")
+    print("\t**    3 - Salir                      **")
     print("\t**                                   **")
     print("\t**                                   **")
     print("\t***************************************")
@@ -21,42 +27,21 @@ def pintarMenu():
     return input("\tDigite una opcion: ")
 
 
+def leerRuta():
+    while(flag == True):
+        time.sleep(1)
+        if(os.path.exists('ruta.txt')):
+            util.escribirArchivo('bitacora.txt', '\n[1,2,3,0]')
+            time.sleep(5)
+            os.remove("ruta.txt")
+
+
 if __name__ == '__main__':
-    if (pintarMenu() == '1'):
-        ruta = input("\tDigite la ruta: ")
-        util.escribirArchivo('ruta.txt','')
-
-import socket
-import threading
-
-def iniciarServidor(c, addr):       
-    
-    print("Se estableció conexion con: %s" % str(addr))
-    
-    msg_rec= c.recv(1024)
-    sucesos=[]
-    direccion=[]
-    mensaje=msg_rec.decode("utf-8").split(',')
-    for x in range(len(mensaje)):
-        if x<len(mensaje)-4:
-            direccion.append(mensaje[x])
-        else:
-            sucesos.append(mensaje[x])
-
-    print(f"Direccion:  {direccion}")
-    print(f"Sucesos:  {direccion}")
-    c.close()
-
-if __name__ == "__main__":
-    
-    host=""
-    puerto=7000
-    s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((host, puerto))
-    s.listen(5)
-
-    while True:
-        (c, addr)=s.accept()
-        hilo=threading.Thread(iniciarServidor(c, addr))
-        hilo.start()
-        print(f"Hilos:  {threading.active_count()}")
+    hiloCLiente = threading.Thread(target=leerRuta)
+    hiloCLiente.start()
+    while(flag == True):
+        if (pintarMenu() == '1'):
+            ruta = input("\tDigite la ruta: ")
+            util.escribirArchivo('ruta.txt', ruta)
+        elif (pintarMenu() == '1'):
+            flag = False
